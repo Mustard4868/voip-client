6e2ddd80032046607ed0f8538dd9a430e973fd74
Mustard4868 <102016032+Mustard4868@users.noreply.github.com>
2024-01-11 13:42:05 +0100

    create voice.py

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import socket
 import threading
 import sqlite3
+import sounddevice as sd
+import numpy as np
 
 self_ip = socket.gethostbyname(socket.gethostname())
 destination_ip = "127.0.0.1"
 port = 5000
+
+samplerate = 48000
