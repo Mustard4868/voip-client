d4857236dc7f6e1604ad839050a6abd77f9e970b
Navoddakshine <96253618+Navoddakshine@users.noreply.github.com>
2024-01-30 19:45:27 +0100

    Add files via upload

---
+++ filtering.py
@@ -0,0 +1,18 @@
+from pydub import AudioSegment
+import numpy as np
+
+data = []
+data.append(np.random.rand(44100))
+i = 0
+def filter_audio(data):
+    filtered_data = []
+    for audio_segment in data:
+        audio_segment = AudioSegment(audio_segment.tobytes(), sample_width=audio_segment.dtype.itemsize, frame_rate=44100, channels=1)
+        filtered_audio = audio_segment.low_pass_filter(1000)
+        filteredaudionump = np.array(filtered_audio.get_array_of_samples(), dtype=np.float32)
+        filtered_data.append(filteredaudionump)
+    return filtered_data
+
+filtered_data = filter_audio(data)
+ #print(filtered_data)
+
