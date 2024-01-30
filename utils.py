bfd913f7754f452eea205544054a548396678da9
Mustard <102016032+Mustard4868@users.noreply.github.com>
2024-01-30 12:10:29 +0100

    change socket(s)

--- utils.py
+++ utils.py
@@ -2,17 +2,17 @@
 import socket
 import random
 import threading
-import multiprocessing
 
 """ Global variables. """
-port = 5060
+r_port = 5060
+s_port = 23456
 self_ip = socket.gethostbyname(socket.gethostname())
 
 destination_ip = "192.168.2.167"
 
 """ Port configuration. """
-udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-udp_sock.bind((self_ip, port))
-udp_sock.connect((self_ip, port))
-print(f"Connected to: {udp_sock.getsockname()}")
+receive_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+receive_sock.bind((self_ip, r_port))
+
+transmit_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+transmit_sock.bind((self_ip, t_port))
