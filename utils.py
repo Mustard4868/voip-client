19eab8fc65c4695b8ed49309babfc2820812a13e
Mustard4868 <102016032+Mustard4868@users.noreply.github.com>
2024-02-01 17:39:54 +0100

    configure sockets

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
 """ Import modules. """
 import threading
 import pyaudio
+import socket
 
+""" Define sockets. """
+class voipSockets(object):
+
+    def __init__(self):
+        self.UDP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.TCP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
