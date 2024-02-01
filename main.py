51a71163d4d329a45ff165e0ce6d3ce14acb41b1
Mustard4868 <102016032+Mustard4868@users.noreply.github.com>
2024-02-01 16:51:39 +0100

    structure main
    added basic structure to main,
    removed unused files,
    added .db filetypes to gitignore

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from utils import *
+
+class voipThreads:
+    def __init__(self):
+
+        receive = threading.Thread(target = receive_data).start()
+
+
 def main():
     
-
-
-
-
+    voipThreads.receive.start()
 
 if __name__ == "__main__":
     main()
