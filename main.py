e841bd8c2d16e5ed789efbfeb06df211ab1182eb
Mustard4868 <102016032+Mustard4868@users.noreply.github.com>
2024-02-01 16:42:40 +0100

    prepare new frame

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from utils import *
-from comm import receive_data, transmit_data, transmit_voice
-
-receive_data_thread = threading.Thread(target=receive_data)
-transmit_data_thread = threading.Thread(target=transmit_data)
-
 def main():
+    
 
-    receive_data_thread.start()
+
+
+
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,24 +1,3 @@
 """ Import modules. """
-import socket
-import random
 import threading
-import wave
-import numpy as np
-import sounddevice as sd
-
-""" Global variables. """
-samplerate = 44100
-
-r_port = 5060
-t_port = 23456
-self_ip = socket.gethostbyname(socket.gethostname())
-
-destination_ip = "192.168.2.101"
-destination_addr = (destination_ip, r_port)
-
-""" Port configuration. """
-receive_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-receive_sock.bind((self_ip, r_port))
-
-transmit_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-transmit_sock.bind((self_ip, t_port))
+import pyaudio
